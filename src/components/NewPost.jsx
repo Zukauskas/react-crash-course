2ce7b02d1b61859a16d1dca8b22dbcf74f52d51d
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-10 18:13:12 +0200

    UseState

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,15 @@
 import styles from './NewPost.module.css';
 
-export function NewPost() {
+export function NewPost({ onBodyChange, onAuthorChange }) {
     return (
         <form className={styles.form}>
             <p>
                 <label htmlFor="author">Author</label>
-                <input id="author" type="text" required />
+                <input id="author" type="text" required onChange={onAuthorChange} />
             </p>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" rows={3} required></textarea>
+                <textarea id="body" rows={3} required onChange={onBodyChange}></textarea>
             </p>
         </form>
     );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import { NewPost } from './NewPost';
 import { Post } from './Post';
 import styles from './PostsList.module.css';
 
 export function PostsList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
     return (
         <>
-            <NewPost />
+            <Modal>
+                <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+            </Modal>
             <ul className={styles.posts}>
-                <Post author="Tautvydas" body="React.js is awesome!" />
+                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Morphius" body="You are the chosen One, Neo" />
             </ul>
         </>
